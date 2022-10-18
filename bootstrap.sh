4797f8d87fe430407f91d99188b46f928a49b574
Nabeel Sherazi <nabeelsherazi97@gmail.com>
2022-10-18 03:24:32 -0700

    add kitware ppa for cmake

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,6 +35,12 @@ function is_executable_available {
     [ -x "$(command -v "$1")" ];
 }
 
+# --- Add PPAs for newer versions than available on main repository ---
+
+# CMake from Kitware PPA
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+
 # --- Check for missing APT packages ---
 
 for pkg in "${packages_to_check[@]}"; do
