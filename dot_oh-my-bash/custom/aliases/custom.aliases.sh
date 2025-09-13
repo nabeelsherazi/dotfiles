282845002fa99d2412c9d833641d2465447af7be
Nabeel Sherazi <nabeelsherazi97@gmail.com>
2025-09-12 23:33:50 -0700

    initial commit

---
+++ dot_oh-my-bash/custom/aliases/custom.aliases.sh
@@ -0,0 +1,8 @@
+# Add your own custom alias in the custom/aliases directory. Aliases placed
+# here will override ones with the same name in the main alias directory.
+
+# you better not have more than one ROS installation
+alias ros2dev="source /opt/ros/*/setup.bash"
+
+# jetsons
+alias jetson="tio /dev/ttyUSB0 -b 115200"
